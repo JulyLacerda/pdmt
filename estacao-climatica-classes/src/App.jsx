f7ccc53fe904648392b3777edc21a44a021ddead
Juliana Lacerda <july.moraes85@gmail.com>
2025-03-14 00:00:54 -0300

    learn(ap05-react) encerrra a secao 2.8

--- estacao-climatica-classes/src/App.jsx
+++ estacao-climatica-classes/src/App.jsx
@@ -7,9 +7,60 @@ class App extends React.Component {
       longitude: null,
       estacao: null,
       data: null,
-      icona: null
+      icone: null
     }
   }
+
+  obterEstacao = (data,latitude) => {
+    const anoAtual = data.getFullYear()
+    //21/06
+    const d1 = new Date(anoAtual, 5, 21)
+    //24/09
+    const d2 = new Date(anoAtual, 8, 24)
+    //21/12
+    const d3 = new Date(anoAtual, 11, 21)
+    //21/03
+    const d4 = new Date(anoAtual, 2, 22)
+    const estaNoSul = latitute < 0
+    if (data >= d1 && data < d2)
+      return estaNoSul ? 'Inverno' : 'Verão'
+    if (data >= d2 && data < d3)
+      return estaNoSul ? 'Primavera' : 'Outono'
+    if (data >= d3 || data < d4)
+      return estaNoSul ? 'Verão' : 'Inverno'
+    return estaNoSul ? 'Outono' : 'Primavera'
+  
+  }
+
+  icones = {
+    'Primavera' : 'seedling',
+    'Verão' : 'clouds-sun',
+    'Outono' : 'tree',
+    'Inverno' : 'snowflake',
+
+  }  
+
+  obterLocalizacao = () => {
+    navigator.geolocation.getCurrentPosition(
+      (posicao) => {
+        const dataAtual = new Date()
+        const estacao = this.obterEstacao(dataAtual, posicao.coords.latitude)
+        const icone = this.icones[estacao]
+        this.setState ({
+          latitude: posicao.coords.latitude,
+          longitude: posicao.coords.longitude,
+          estacao: estacao,
+          data: dataAtual.toLocaleTimeString(),
+          icone: icone
+        })
+      },
+      (erro) => {
+
+      }
+
+    )
+  }
+
   render(){
   return (
     <div>Meu App</div>
